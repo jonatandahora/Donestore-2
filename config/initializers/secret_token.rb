4b5500a47d7c153aa1c7289e1da7bc2bb0f972c4
jonatan <jonatandahora@gmail.com>
2013-12-08 04:28:57 -0200

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DoneStore20::Application.config.secret_key_base = '82ff344fea8a5e4ca2eeb3c8d0564571898e08fbfcf10a05002722f900a5535d500d8b0a6abc775e03006d2134f2fefed73df5bddaea3dcac9503d5889f065c1'
